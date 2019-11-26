2aa35bda0cc1e083d9f9a6172eb5529a2513fa28
Shohanur Rahman <shohan.mbstu@gmail.com>
2019-11-26 12:53:00 +0600

    Model relationship

--- api_example/languages/views.py
+++ api_example/languages/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Language
-from .serializers import LanguageSerializer
+from .models import *
+from .serializers import *
 
 class LanguageView(viewsets.ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+
+
+class ParadigmView(viewsets.ModelViewSet):
+    queryset = Paradigm.objects.all()
+    serializer_class = ParadigmSerializer
+
+
+class ProgrammerView(viewsets.ModelViewSet):
+    queryset = Programmer.objects.all()
+    serializer_class = ProgrammerSerializer
